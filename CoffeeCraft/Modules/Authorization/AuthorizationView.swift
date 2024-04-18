d4425fda39c5e7c883cdd8b855f39d4170f78be5
turgunbaevaa <nyc.aruu@gmail.com>
2024-04-18 16:28:03 +0200

    Getting started

---
+++ CoffeeCraft/Modules/Authorization/AuthorizationView.swift
@@ -0,0 +1,20 @@
+//
+//  AuthorizationView.swift
+//  CoffeeCraft
+//
+//  Created by Aruuke Turgunbaeva on 18/4/24.
+//
+
+import UIKit
+
+class AuthorizationView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ CoffeeCraft/Modules/Start/StartView.swift
@@ -0,0 +1,20 @@
+//
+//  StartView.swift
+//  CoffeeCraft
+//
+//  Created by Aruuke Turgunbaeva on 18/4/24.
+//
+
+import UIKit
+
+class StartView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
